fa2a970336dd9695e732578b15a5b5e4f57f1704
Michał <michal.talaga.programming@gmail.com>
2022-11-20 16:27:16 +0100

    update

--- import os.py
+++ import os.py
@@ -1,7 +1,7 @@
 import os
 import random as rand
 
-hasla=[
+hasla = [
     "AUDI",
     "BMW",
     "MERCEDES",
@@ -22,28 +22,28 @@ SZUBIENICA = ['''
 
 
 
-''','''
+''', '''
 
 
 
 
 
 
-=========''','''
+=========''', '''
   
       |
       |
       |
       |
       |
-=========''','''
+=========''', '''
   +---+
       |
       |
       |
       |
       |
-=========''','''
+=========''', '''
   +---+
   |   |
       |
@@ -94,18 +94,56 @@ SZUBIENICA = ['''
       |
 =========''']
 
-    
-
 
 def wisielec(szanse):
     os.system("cls")
     print(SZUBIENICA[szanse])
-            
+
+
+def losowanie_hasla():
+    wylosowane_haslo = rand.choice(hasla)
+    return wylosowane_haslo
+
+
+def narysuj_haslo(haslo, znaki):
+    for i in range(len(haslo)):
+        if haslo[i] in znaki:
+            print(haslo[i], end=" ")
+        else:
+            print("_", end=" ")
+    print()
+
+
+def main():
+    wylosowane_haslo = losowanie_hasla()
+    gra(wylosowane_haslo)
+
+
 def gra(wylosowane_haslo):
-    
-    dlugosc = wylosowane_haslo.__len__() 
+
+    dlugosc = wylosowane_haslo.__len__()
     wisielec(0)
+    print("Witaj w grze wisielec")
+    print("Twoje haslo ma", dlugosc, "liter")
+    print("Powodzenia")
+    print()
+    znaki = set()
+    szanse = 0
+    while szanse < 10:
 
-
-
-gra(rand.choice(hasla))
+        narysuj_haslo(wylosowane_haslo, znaki)
+        print("Podaj litere:")
+        litera = input().upper()
+        if litera in znaki:
+            print("Ta litera juz byla")
+        elif litera in wylosowane_haslo:
+            print("Zgadles")
+            znaki.add(litera)
+            if znaki == set(wylosowane_haslo):
+                print("Brawo wygrales")
+                break
+        else:
+            print("Nie zgadles")
+            szanse += 1
+            wisielec(szanse)
+            znaki.add(litera)
